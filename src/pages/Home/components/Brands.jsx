2b0585118b68d7c73b1a213b4a05c93a32472ac9
mb8504 <mbergmarketing@gmail.com>
2024-07-26 05:15:42 -0400

    updated Brands section on homepage

--- src/pages/Home/components/Brands.jsx
+++ src/pages/Home/components/Brands.jsx
@@ -8,11 +8,11 @@ function Brands() {
                 Our Clients
             </div>
             <ul className='grid lg:grid-cols-6 md:grid-cols-4 smGrid'>
-                <li className='font-sans text-xl'>BioInsights</li>
-                <li className='font-sans text-xl'>BioInsights</li>
-                <li className='font-sans text-xl'>BioInsights</li>
-                <li className='font-sans text-xl'>BioInsights</li>
-                <li className='font-sans text-xl'>BioInsights</li>
+                <li className='font-sans text-xl'><img src="public/makeupbrand.png" alt="" /></li>
+                <li className='font-sans text-xl'><img src="public/clothingbrand.png" alt="" /></li>
+                <li className='font-sans text-xl'><img src="public/icecreambrand.png" alt="" /></li>
+                <li className='font-sans text-xl'><img src="public/recordbrand.png" alt="" /></li>
+                <li className='font-sans text-xl'><img src="src/assets/photobrand.png" alt="" /></li>
                 <li className='font-sans text-xl'>BioInsights</li>
             </ul>
         </div>
